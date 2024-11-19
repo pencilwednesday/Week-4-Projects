8e90a7d35870549e13176fb34ed80c3a5308977b
pencilwednesday <112817872+pencilwednesday@users.noreply.github.com>
2024-11-18 16:18:46 -0800

    save prog

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 // template_dr7y6d7
 // service_o3e6n0g
 // TOTDZC7oKcYj2kEke
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_o3e6n0g",
+      "template_dr7y6d7",
+      event.target,
+      "TOTDZC7oKcYj2kEke"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is templorarily unavailable. Please contact me directly on pencilwednesday@yahoo.com"
+      );
+    });
+}
+
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
